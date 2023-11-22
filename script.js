15fcade5ff52345d782391eb050f20e648f70d5a
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-22 15:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 function firstChar(text) {
   // your code here
-	 if(text===' ' || text==='') return '';
-	
-	if(text.charAt(0)===' '){
-		return text.charAt(1);
-	}else{
-		return text.charAt(0);
-	}
+  // Trim the input string to remove leading and trailing spaces
+  const trimmedString = inputString.trim();
 
-	
-	
-	
-
+  // Check if the trimmed string is empty
+  if (trimmedString === '') {
+    return '';
   }
 
+  // Return the first character of the trimmed string
+  return trimmedString[0];
+}
 
 
 
-
-	
 // Do not change the code below
 
 const text = prompt("Enter text:");
