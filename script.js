c74896eb09c563fc233dbde6fc2beb54e4096003
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-22 14:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function firstChar(text) {
   // your code here
-}
+	 if(text===' ' || text==='') return '';
+	
+	if(text.charAt(0)===' '){
+		return text.charAt(1);
+	}else{
+		return text.charAt(0);
+	}
 
+	
+	
+	
+
+  }
+
+
+
+
+
+	
 // Do not change the code below
 
 const text = prompt("Enter text:");
